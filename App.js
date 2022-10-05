c447b66a7616e25573ba715e290dc4b6091dffb5
mdtamizuddin <tamizrabbi@gmail.com>
2022-10-05 20:46:58 +0600

    project Created

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { StyleSheet, TextInput, View , SafeAreaView} from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const [text, onChangeText] = React.useState("");
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      
+      <WebView
+        source={
+          { uri: `https://dado-chat.mdtamiz.xyz/${text && text.toLocaleLowerCase()}` }
+        } />
+         <TextInput
+        style={styles.inputSearnch}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Write a path to go like ( login , signup )"
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#00000',
+    marginTop : 20
   },
+  inputSearnch: {
+    height: 35,
+    margin: 5,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    borderColor: '#00000',
+    marginTop: 20,
+    marginBottom: 10
+  }
 });
